029a4945cc2c16a9e7989b8fd351eb627a865e3b
Patrick Hund <patrick.hund@adevinta.com>
2025-12-17 16:15:43 +0100

    feat: configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import * as fs from "fs";
+import * as path from "path";
+import * as os from "os";
 
 const program = new Command();
 
 program.action(() => {
-  console.log("hello world");
+  console.log("==================");
+  console.log("Simple Mind Search");
+  console.log("==================");
+  const configPath = path.join(os.homedir(), ".simple-mind-search");
+
+  if (!fs.existsSync(configPath)) {
+    const defaultConfigPath = path.join(__dirname, "..", ".simple-mind-search.default");
+    
+    if (!fs.existsSync(defaultConfigPath)) {
+      console.error("Default configuration file not found");
+      process.exit(1);
+    }
+
+    fs.copyFileSync(defaultConfigPath, configPath);
+    console.log(`Created new configuration file: ${configPath}`);
+  }
+
+  const config = fs.readFileSync(configPath, "utf-8");
+  console.log("Using configuration:");
+  console.log(config);
 });
 
 program.parse(process.argv);
